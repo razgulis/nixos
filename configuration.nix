a9a6f9d115bec9e95a2c96bf4fb1d49e3ff9c761
Sergei Razgulin <sergei.razgulin@gmail.com>
2025-02-16 15:19:27 -0700

    Added statit ip address.  Working version

--- configuration.nix
+++ configuration.nix
@@ -17,10 +17,10 @@
   
   networking = {
     hostName = "nixos";
-    #interfaces.end0.ipv4.addresses = [{
-    #  address = "192.168.1.200";
-    #  prefixLength = 24;
-    #}];
+    interfaces.end0.ipv4.addresses = [{
+      address = "192.168.1.200";
+      prefixLength = 16;
+    }];
     defaultGateway = "192.168.1.1";
     nameservers = [ "1.1.1.1" "8.8.8.8" ];
     firewall.allowedTCPPorts = [ 22 53 ];
